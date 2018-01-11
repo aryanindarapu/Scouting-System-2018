4743d96877d19e6d2747f2f0a10322060539346e
rm12480 <32555115+rm12480@users.noreply.github.com>
2018-01-11 18:17:00 -0500

    1/11/18
    Ray Mo

--- scoutingSystem.js
+++ scoutingSystem.js
@@ -14,7 +14,39 @@ function openTab(evt, tabName) {
 
 var idArray = ["foul", "tech"]
 function add(n, id) {
-	x = parseInt(document.getElementById(fouls[id]).innerHTML);
+	x = parseInt(document.getElementById(idArray[id]).innerHTML);
 	x += n;
-	document.getElementById(fouls[id]).innerHTML = x.toString();
+	if (x<0) {
+		x=0
+	}
+	document.getElementById(idArray[id]).innerHTML = x.toString();
+}
+
+function submit() {
+	teamName = document.getElementById("teamName").value;
+	console.log(teamName);
+	teamNo = document.getElementById("teamNo").value;
+	console.log(teamNo);
+	cross = document.getElementById("cross").checked;
+	console.log(cross);
+	switchvar = document.getElementById("switch").checked;
+	console.log(switchvar);
+	scale = document.getElementById("scale").checked;
+	console.log(scale);
+	foul = document.getElementById("foul").innerHTML;
+	console.log(foul);
+	tech = document.getElementById("tech").innerHTML;
+	console.log(tech);
+	yellow = document.getElementById("yellow").checked;
+	console.log(yellow);
+	red = document.getElementById("red").checked;
+	console.log(red);
+	attempt = document.getElementById("attempt").checked;
+	console.log(attempt);
+	fail = document.getElementById("fail").checked;
+	console.log(fail);
+	park = document.getElementById("park").checked;
+	console.log(park);
+	comment = document.getElementById("comment").value;
+	console.log(comment);
 }
